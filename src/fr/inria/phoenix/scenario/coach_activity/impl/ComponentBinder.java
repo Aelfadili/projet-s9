8ad6555c9737acea4eb2ef75ba0c7e6be04aa65e
Aelfadili <amine.elfadili@yahoo.fr>
2017-11-24 15:01:20 +0100

    version 2 bg

--- src/fr/inria/phoenix/scenario/coach_activity/impl/ComponentBinder.java
+++ src/fr/inria/phoenix/scenario/coach_activity/impl/ComponentBinder.java
@@ -1,5 +1,9 @@
 package fr.inria.phoenix.scenario.coach_activity.impl;
         
+import fr.inria.phoenix.diasuite.framework.context.alertmove.AbstractAlertMove;
+import fr.inria.phoenix.diasuite.framework.context.congratulation.AbstractCongratulation;
+import fr.inria.phoenix.diasuite.framework.context.propositiongoout.AbstractPropositionGoOut;
+import fr.inria.phoenix.diasuite.framework.controller.coach_activitycontroller.AbstractCoach_activityController;
 import fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder;
 
 /* (non-Javadoc)
@@ -7,4 +11,29 @@ import fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder;
  * @see fr.inria.phoenix.diasuite.framework.misc.AppComponentBinder
  */
 public class ComponentBinder extends AppComponentBinder {
+
+	@Override
+	public Class<? extends AbstractAlertMove> getAlertMoveClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends AbstractCongratulation> getCongratulationClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Class<? extends AbstractPropositionGoOut> getPropositionGoOutClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends AbstractCoach_activityController> getCoach_activityControllerClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
